2e9b66c252a075c4206f7254b9be22f82945c791
Rabeaa <rabeaa.kessal@gmail.com>
2020-10-09 12:20:44 +0200

    functional rateOfSuccess (successCount = 330 at the end)

--- src/main/java/fr/istic/pdl1819_grp5/TestMain.java
+++ src/main/java/fr/istic/pdl1819_grp5/TestMain.java
@@ -19,12 +19,16 @@ public class TestMain {
         File fileHtml = new File("output\\html");
         File fileWiki = new File("output\\wikitext");
         File[] filesHtml = fileHtml.listFiles();
+        System.out.println(filesHtml.length);
         File[] filesWikitext = fileWiki.listFiles();
+        System.out.println(filesWikitext.length);
         for(int i=0; i<filesHtml.length; i++){
+            //System.out.println("je suis i = " + i);
             for(int j=0; j<filesWikitext.length; j++) {
-
+                //System.out.println("je suis j = " + j);
                 if(rateOfSuccessAux(filesHtml[i],filesWikitext[j])) {
                     successCount++;
+                    System.out.println(successCount);
                 }
             }
         }
